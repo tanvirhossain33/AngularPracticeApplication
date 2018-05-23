128a08b5daec18cf3e63b3bbf70d3234dd21fcdf
DESKTOP-FT0LQ52\Mehedi <mehedi_60@ymail.com>
2018-05-23 13:34:43 +0600

    add DateTime Picker and Alert component in this application

--- AngularApplication/Scripts/app/app.js
+++ AngularApplication/Scripts/app/app.js
@@ -1,11 +1,12 @@
-var myApp = angular.module('myApp', ['ui.router']);
+var myApp = angular.module('myApp', [
+    'ui.router',
+    'ui.bootstrap'
+]);
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
-
     $stateProvider
-
         .state('home', {
             url: '/home',
             templateUrl: 'partial-home.html'
@@ -13,55 +14,99 @@ myApp.config(function ($stateProvider, $urlRouterProvider) {
         .state('home.list', {
             url: '/list',
             templateUrl: 'partial-home-list.html',
-            controller: function($scope){
+            controller: function ($scope) {
                 $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
             }
         })
-
         .state('home.paragraph', {
             url: '/paragraph',
             template: 'I could sure use a drink right now.'
         })
-
-        
         .state('about', {
             url: '/about',
-            
             views: {
-                
                 '': { templateUrl: 'partial-about.html' },
-
-               
                 'columnOne@about': { template: 'Look I am a column !' },
-
-                
                 'columnTwo@about': {
                     templateUrl: 'table-data.html',
                     controller: 'scotchController'
                 }
             }
         })
-
-
-
         .state('datePicker', {
             url: '/datePicker',
             templateUrl: 'partials/datePicker.html',
             controller: 'DatePickerCtrl'
+        })
+        .state('alert', {
+            url: '/alert',
+            templateUrl: 'partials/alert.html',
+            controller: 'AlertCtrl'
         });
 });
 
+myApp.controller('AlertCtrl', function ($scope) {
+    $scope.message = 'Angular UI Bootstrap Alert Componenet Example';
+    $scope.alerts = [
+          { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+          { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+    ];
 
-myApp.controller('DatePickerCtrl', function ($scope) {
+    $scope.addAlert = function () {
+        $scope.alerts.push({ msg: 'Another alert!' });
+    };
 
-
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+    };
 });
 
+myApp.controller('TimepickerCtrl', function ($scope) {
+    $scope.mytime = new Date();
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+    $scope.options = {
+        hstep: [1, 2, 3],
+        mstep: [1, 5, 10, 15, 25, 30]
+    };
+
+    $scope.ismeridian = true;
+    $scope.toggleMode = function () {
+        $scope.ismeridian = !$scope.ismeridian;
+    };
+
+    $scope.update = function () {
+        var d = new Date();
+        d.setHours(14);
+        d.setMinutes(0);
+        $scope.mytime = d;
+    };
+
+    $scope.changed = function () {
+        $log.log('Time changed to: ' + $scope.mytime);
+    };
+
+    $scope.clear = function () {
+        $scope.mytime = null;
+    };
+});
+
+myApp.controller('DatePickerCtrl', function ($scope) {
+    $scope.date = new Date();
+
+    $scope.status = {
+        opened: false
+    };
+
+    $scope.open = function ($event) {
+        $scope.status.opened = true;
+    };
+
+    $scope.dateFormat = "dd/mm/yyyy";
+});
 
 myApp.controller('scotchController', function ($scope) {
-
     $scope.message = 'List of Scotches';
-
     $scope.scotches = [
         {
             name: 'Macallan 12',
@@ -76,5 +121,4 @@ myApp.controller('scotchController', function ($scope) {
             price: 20000
         }
     ];
-
 });
